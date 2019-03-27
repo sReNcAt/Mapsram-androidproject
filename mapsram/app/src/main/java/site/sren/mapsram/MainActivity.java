8bd70e095f50f57253fd297993342642ce2687bc
SDongJoon <srencat@naver.com>
2019-03-28 06:42:52 +0900

    SQLite 사용을위한 기본 DB 생성 및 CRUD 빌드

--- mapsram/app/src/main/java/site/sren/mapsram/MainActivity.java
+++ mapsram/app/src/main/java/site/sren/mapsram/MainActivity.java
@@ -1,6 +1,9 @@
 package site.sren.mapsram;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -17,6 +20,11 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     final static ArrayList<String> items = new ArrayList<String>() ;
+    private SQLiteHelper helper;
+    String dbName = "alram.db";
+    int dbVersion = 1;
+    private SQLiteDatabase db;
+    String tag = "SQLite";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +34,24 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+        //SQLite 객체 생성
+        helper = new SQLiteHelper(this,dbName,null,dbVersion);
+        try {
+            db = helper.getWritableDatabase(); // 읽고 쓸수 있는 DB
+            //db = helper.getReadableDatabase(); // 읽기 전용 DB select문
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+            Log.e(tag, "데이터베이스를 얻어올 수 없음");
+            finish(); // 액티비티 종료
+        }
+
         // ArrayAdapter 생성. 아이템 View를 선택(single choice)가능하도록 만듦.
         final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, items) ;
 
         // listview 생성 및 adapter 지정.
         final ListView listview = (ListView) findViewById(R.id.listview1) ;
         listview.setAdapter(adapter) ;
+
         /*
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -130,4 +150,31 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    // SQLite 제어를 위한 CRUD
+    void SQLite_delete() {
+        db.execSQL("delete from alram where id=1;");
+        Log.d(tag, "delete 완료");
+    }
+
+    void SQLite_update() {
+        db.execSQL("update alram set id='0' where id=1;");
+        Log.d(tag, "update 완료");
+    }
+
+    void SQLite_select() {
+        Cursor c = db.rawQuery("select * from alram;", null);
+        while(c.moveToNext()) {
+            int id = c.getInt(0);
+            String name = c.getString(1);
+            Log.d(tag,"id:"+id+",name:"+name);
+        }
+    }
+
+    void SQLite_insert () {
+        db.execSQL("insert into alram (id) values(1);");
+        Log.d(tag, "insert 성공");
+    }
+
 }
